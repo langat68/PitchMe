6041aec70f3435c7442e7abd9dd6e4ee101ac756
langat68 <langatfidel3@gmail.com>
2025-10-04 22:19:38 +0300

    first commit

---
+++ pitchme/src/app.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'preact/hooks'
+import preactLogo from './assets/preact.svg'
+import viteLogo from '/vite.svg'
+//import './app.css'
+import Hero from "./components/Hero"
+
+export function App() {
+  
+
+  return (
+    <>
+      
+      <Hero/>
+      
+    </>
+  )
+}
---
+++ pitchme/src/components/Hero.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div>
+      <p>Tunaunda cv wapi kazi😂😂🐼</p>
+    </div>
+  )
+}
+
+export default Hero
